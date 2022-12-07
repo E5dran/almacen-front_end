a8a080dfa3ea7f322b09606c5daead2cb302336a
lainvers <2012yvonneg28@gmail.com>
2022-12-07 16:24:49 +0100

    interface creado por error en nombre un creación del nuevo

---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,14 @@
+export interface User {
+
+    name: string;
+    surname: string;
+    email: string;
+    phone: string;
+    adress: string;
+    dni: string;
+    gender: string;
+    status: string;
+    category: string;
+
+
+}
