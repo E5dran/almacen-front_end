bfce183106ab8b7c879250edd24c57b331ce016e
e5dran <esdran.bc@gmail.com>
2022-12-12 19:21:55 +0100

    Creacion componente nav jefe

--- src/app/components/nav/nav-jefe/nav-jefe.component.ts
+++ src/app/components/nav/nav-jefe/nav-jefe.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nav-jefe',
+  selector: 'nav-jefe',
   templateUrl: './nav-jefe.component.html',
   styleUrls: ['./nav-jefe.component.css']
 })
 export class NavJefeComponent implements OnInit {
 
-  constructor() { }
+  sidebarWidth: string;
+  activo: boolean;
+  inactivo: boolean;
+  nombreEncargado: string
+
+  color: string;
+  backgroundColor: string;
+
+  constructor() {
+    this.sidebarWidth = '0px';
+    this.activo = false;
+    this.inactivo = true;
+    this.nombreEncargado = 'Oscar';
+
+    this.backgroundColor = 'rgb(6, 78, 75)';
+    this.color = 'white';
+  }
 
   ngOnInit(): void {
+
+    if (window.innerWidth <= 768) {
+      this.sidebarWidth = '50px';
+      this.backgroundColor = 'white';
+      this.color = 'rgb(6, 78, 75)';
+    } else {
+      this.sidebarWidth = '200px';
+    }
+  }
+
+  toggleSidebar() {
+    if (this.sidebarWidth === '50px') {
+      this.sidebarWidth = '200px';
+      this.backgroundColor = 'rgb(6, 78, 75)';
+      this.color = 'white';
+    } else {
+      this.sidebarWidth = '50px';
+      this.backgroundColor = 'white'
+      this.color = 'rgb(6, 78, 75)';
+    }
+    this.activo = !this.activo
+    this.inactivo = !this.inactivo
   }
 
 }
