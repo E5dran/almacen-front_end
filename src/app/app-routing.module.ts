de582241c62cee91b840a40f396a9788b7c5f9d2
Oscar Rolón <oscarpcpi2gmai@gmail.com>
2022-12-12 10:49:21 +0100

    Mergeo de la rama login, nav encargado y creacion de encargad overview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
+import { LoginComponent } from './components/login/login.component';
 import { OverviewComponent } from './components/encargado/overview/overview.component';
 const routes: Routes = [
   { path: 'encargado/overview', component: OverviewComponent },
-
-];
-=======
-import { LoginComponent } from './components/login/login.component';
-
-const routes: Routes = [
-
   { path: 'login', component: LoginComponent }
 
 ];
 
->>>>>>> componentes-login
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { NavEncargadoComponent } from './components/encargado/nav-encargado/nav-encargado.component';
 import { OverviewComponent } from './components/encargado/overview/overview.component';
-=======
 import { LoginComponent } from './components/login/login.component';
->>>>>>> componentes-login
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     NavEncargadoComponent,
-    OverviewComponent
-=======
+    OverviewComponent,
     LoginComponent
->>>>>>> componentes-login
   ],
   imports: [
     BrowserModule,
