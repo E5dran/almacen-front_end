f608e9d71689c733a5eda55d2f9187d4acd2e9f2
piki1489 <piki1489@hotmail.com>
2022-12-13 09:59:23 +0100

    create-order terminado a falta de los servers

--- src/app/components/operario/create-order/create-order.component.ts
+++ src/app/components/operario/create-order/create-order.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-create-order',
   templateUrl: './create-order.component.html',
@@ -7,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateOrderComponent implements OnInit {
 
-  constructor() { }
+  createOrder: FormGroup;
+
+  constructor() {
+    this.createOrder = new FormGroup({
+      Client: new FormControl(),
+      Description: new FormControl(),
+      N_Items: new FormControl(),
+      Destination_Address: new FormControl(),
+      Truck_Plate: new FormControl(),
+      Departure_Date: new FormControl(),
+      Arrival_Date: new FormControl()
+    });
+
+
+  }
 
   ngOnInit(): void {
   }
