5c13999eabf38208c3c931a8aaca006ce600855d
e5dran <esdran.bc@gmail.com>
2022-12-13 09:30:50 +0100

    UPDATE eliminado nav-encargado antiguo y actualizada la importacion del componente en app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavEncargadoComponent } from './components/encargado/nav-encargado/nav-encargado.component';
 import { OverviewComponent } from './components/encargado/overview/overview.component';
 import { LoginComponent } from './components/login/login.component';
 
@@ -14,6 +13,7 @@ import { JefeoverviewComponent } from './components/jefe/overview/jefeoverview.c
 import { NewUserComponent } from './components/jefe/new-user/new-user.component';
 import { NavJefeComponent } from './components/nav/nav-jefe/nav-jefe.component';
 import { NavOperarioComponent } from './components/nav/nav-operario/nav-operario.component';
+import { NavEncargadoComponent } from './components/nav/nav-encargado/nav-encargado.component';
 registerLocaleData(localeEs, 'es');
 
 
@@ -21,12 +21,12 @@ registerLocaleData(localeEs, 'es');
 @NgModule({
   declarations: [
     AppComponent,
-    NavEncargadoComponent,
     OverviewComponent,
     LoginComponent,
     JefeoverviewComponent,
     NewUserComponent,
     NavJefeComponent,
+    NavEncargadoComponent,
     NavOperarioComponent
   ],
   imports: [
