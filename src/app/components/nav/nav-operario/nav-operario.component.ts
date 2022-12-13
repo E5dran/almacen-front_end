6809d8fba276274e1708acb5cafb94ff872af11b
e5dran <esdran.bc@gmail.com>
2022-12-13 09:22:51 +0100

    Creado nuevo nav operario

--- src/app/components/nav/nav-operario/nav-operario.component.ts
+++ src/app/components/nav/nav-operario/nav-operario.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-nav-operario',
+  selector: 'nav-operario',
   templateUrl: './nav-operario.component.html',
   styleUrls: ['./nav-operario.component.css']
 })
 export class NavOperarioComponent implements OnInit {
 
-  constructor() { }
+  sidebarWidth: string;
+  activo: boolean;
+  inactivo: boolean;
+  nombreEncargado: string
+
+  color: string;
+  backgroundColor: string;
+
+  constructor(private router: Router) {
+    this.sidebarWidth = '0px';
+    this.activo = false;
+    this.inactivo = true;
+    this.nombreEncargado = 'Oscar';
+
+    this.backgroundColor = 'rgb(6, 78, 75)';
+    this.color = 'white';
+  }
 
   ngOnInit(): void {
+
+    if (window.innerWidth <= 768) {
+      this.sidebarWidth = '50px';
+      this.backgroundColor = 'white';
+      this.color = 'rgb(6, 78, 75)';
+    } else {
+      this.sidebarWidth = '200px';
+    }
+  }
+
+  toggleSidebar() {
+    if (this.sidebarWidth === '50px') {
+      this.sidebarWidth = '200px';
+      this.backgroundColor = 'rgb(6, 78, 75)';
+      this.color = 'white';
+    } else {
+      this.sidebarWidth = '50px';
+      this.backgroundColor = 'white'
+      this.color = 'rgb(6, 78, 75)';
+    }
+    this.activo = !this.activo
+    this.inactivo = !this.inactivo
   }
 
 }
