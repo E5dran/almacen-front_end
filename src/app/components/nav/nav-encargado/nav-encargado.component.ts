aa16cdc5fca3428080c2e3165a578062537f91c6
e5dran <esdran.bc@gmail.com>
2022-12-13 09:23:55 +0100

    Creado el componente nav encargado

---
+++ src/app/components/nav/nav-encargado/nav-encargado.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nav-encargado',
+  templateUrl: './nav-encargado.component.html',
+  styleUrls: ['./nav-encargado.component.css']
+})
+export class NavEncargadoComponent implements OnInit {
+
+  sidebarWidth: string;
+  activo: boolean;
+  inactivo: boolean;
+  nombreEncargado: string
+
+  color: string;
+  backgroundColor: string;
+
+  constructor() {
+    this.sidebarWidth = '0px';
+    this.activo = false;
+    this.inactivo = true;
+    this.nombreEncargado = 'Oscar';
+
+    this.backgroundColor = 'rgb(6, 78, 75)';
+    this.color = 'white';
+  }
+
+  ngOnInit(): void {
+
+    if (window.innerWidth <= 768) {
+      this.sidebarWidth = '50px';
+      this.backgroundColor = 'white';
+      this.color = 'rgb(6, 78, 75)';
+    } else {
+      this.sidebarWidth = '200px';
+    }
+  }
+
+  toggleSidebar() {
+    if (this.sidebarWidth === '50px') {
+      this.sidebarWidth = '200px';
+      this.backgroundColor = 'rgb(6, 78, 75)';
+      this.color = 'white';
+    } else {
+      this.sidebarWidth = '50px';
+      this.backgroundColor = 'white'
+      this.color = 'rgb(6, 78, 75)';
+    }
+    this.activo = !this.activo
+    this.inactivo = !this.inactivo
+  }
+
+}
