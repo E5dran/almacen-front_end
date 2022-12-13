7837171cd5e95928317dc52605eb99b4701300b7
Oscar Rolón <oscarpcpi2gmai@gmail.com>
2022-12-13 13:55:07 +0100

    componentes de encargado 80% finalizado

--- src/app/components/encargado/overview/overview.component.ts
+++ src/app/components/encargado/overview/overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/interfaces/order.interface';
 
 @Component({
   selector: 'app-overview',
@@ -9,7 +10,23 @@ export class OverviewComponent implements OnInit {
   nombre: string
   apellido: string
   fecha: Date
+  orders: Order[]
   constructor() {
+    this.orders = [{
+      Id: 1,
+      Warehouse_Id: 1,
+      Status: true,
+      Addressee: 'Juan Perez',
+      Description: 'Caja de carton',
+      N_Items: 1,
+      Destination_Address: 'Calle 1',
+      Creation_Date: new Date(),
+      Departure_Date: new Date(),
+      Arrival_Date: new Date(),
+      Truck_Plate: 'ABC123',
+      Category: 'Caja'
+
+    }]
     this.nombre = 'Juan'
     this.apellido = 'Perez'
     this.fecha = new Date()
