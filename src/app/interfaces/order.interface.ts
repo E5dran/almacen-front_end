93a13c88b6d03f8a901301c3811f3beafad0f474
e5dran <esdran.bc@gmail.com>
2022-12-14 14:31:23 +0100

    Modificada interfaz Order y acomodados todos los componentes correspondientes, FIXED problema con la importar un css en index.html

--- src/app/interfaces/order.interface.ts
+++ src/app/interfaces/order.interface.ts
@@ -1,17 +1,17 @@
 export interface Order {
 
-    Id: number,
-    Warehouse_Id: number,
-    Status: boolean,
-    Addressee: string,
-    Description: string,
-    N_Items: number,
-    Destination_Address: string,
-    Creation_Date: Date,
-    Departure_Date: Date,
-    Arrival_Date: Date,
-    Truck_Plate: string,
-    Category: string
-
+    id: number,
+    warehouse_id: number,
+    status: boolean,
+    addressee: string,
+    description: string,
+    n_items: number,
+    destination_address: string,
+    creation_date: Date,
+    departure_date: Date,
+    arrival_date: Date,
+    truck_plate: string,
+    category: string,
+    user_id: string
 
 }
