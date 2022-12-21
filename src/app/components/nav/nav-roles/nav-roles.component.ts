dae60d1f1c3b038847f0ef2825e9bbadeca3d5ea
e5dran <esdran.bc@gmail.com>
2022-12-21 16:39:41 +0100

    Creado el componente nav roles, un navegador unico con las opciones de los demas (ngIf)

---
+++ src/app/components/nav/nav-roles/nav-roles.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import jwt_decode from "jwt-decode";
+
+@Component({
+  selector: 'nav-roles',
+  templateUrl: './nav-roles.component.html',
+  styleUrls: ['./nav-roles.component.css']
+})
+export class NavRolesComponent implements OnInit {
+
+  sidebarWidth: string;
+  activo: boolean;
+  inactivo: boolean;
+  nombre: string
+
+  color: string;
+  backgroundColor: string;
+
+  rol: string;
+
+  constructor(private router: Router) {
+    this.sidebarWidth = '0px';
+    this.activo = false;
+    this.inactivo = true;
+    this.nombre = '';
+
+    this.backgroundColor = 'rgb(6, 78, 75)';
+    this.color = 'white';
+    this.rol = '';
+  }
+
+  ngOnInit(): void {
+    const tokenInfo = jwt_decode(localStorage.getItem('token')!) as any;
+
+    this.nombre = tokenInfo.user_name;
+    this.rol = tokenInfo.user_role;
+
+    if (window.innerWidth <= 768) {
+      this.sidebarWidth = '50px';
+      this.backgroundColor = 'white';
+      this.color = 'rgb(6, 78, 75)';
+    } else {
+      this.sidebarWidth = '200px';
+    }
+  }
+
+  toggleSidebar() {
+    if (this.sidebarWidth === '50px') {
+      this.sidebarWidth = '200px';
+      this.backgroundColor = 'rgb(6, 78, 75)';
+      this.color = 'white';
+    } else {
+      this.sidebarWidth = '50px';
+      this.backgroundColor = 'white'
+      this.color = 'rgb(6, 78, 75)';
+    }
+    this.activo = !this.activo
+    this.inactivo = !this.inactivo
+  }
+
+  logout() {
+    if (confirm("¿Estás seguro de querer cerrar sesión?")) {
+      localStorage.removeItem('token');
+      this.router.navigate(['/login']);
+    }
+  }
+}
